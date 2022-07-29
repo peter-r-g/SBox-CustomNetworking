5e0653100ff5361baa85a16c36fe47257ae6478d
Peter <petergorman11@gmail.com>
2022-07-29 08:44:21 -0400

    Cleanup To.cs

--- Server/Client/To.cs
+++ Server/Client/To.cs
@@ -7,25 +7,25 @@ namespace CustomNetworking.Server;
 
 public struct To : IEnumerable<INetworkClient>
 {
-	public INetworkClient? SingleClient;
-	public IEnumerable<INetworkClient>? MultipleClients;
-	public IEnumerable<INetworkClient>? IgnoredClients;
+	private INetworkClient? _singleClient;
+	private IEnumerable<INetworkClient>? _multipleClients;
+	private IEnumerable<INetworkClient>? _ignoredClients;
 	
 	public static To All => Multiple( NetworkManager.Clients.Values );
 
 	public static To Single( INetworkClient client )
 	{
-		return new To {SingleClient = client};
+		return new To {_singleClient = client};
 	}
 
 	public static To Multiple( IEnumerable<INetworkClient> clients )
 	{
-		return new To {MultipleClients = clients};
+		return new To {_multipleClients = clients};
 	}
 
 	public static To AllExcept( IEnumerable<INetworkClient> clientsToIgnore )
 	{
-		return All with {IgnoredClients = clientsToIgnore};
+		return All with {_ignoredClients = clientsToIgnore};
 	}
 
 	public static To AllExcept( params INetworkClient[] clientsToIgnore )
@@ -35,15 +35,15 @@ public struct To : IEnumerable<INetworkClient>
 
 	public IEnumerator<INetworkClient> GetEnumerator()
 	{
-		if ( SingleClient is not null )
-			yield return SingleClient;
+		if ( _singleClient is not null )
+			yield return _singleClient;
 
-		if ( MultipleClients is null )
+		if ( _multipleClients is null )
 			yield break;
 
-		foreach ( var client in MultipleClients )
+		foreach ( var client in _multipleClients )
 		{
-			if ( IgnoredClients is null || !IgnoredClients.Contains( client ) )
+			if ( _ignoredClients is null || !_ignoredClients.Contains( client ) )
 				yield return client;
 		}
 	}
