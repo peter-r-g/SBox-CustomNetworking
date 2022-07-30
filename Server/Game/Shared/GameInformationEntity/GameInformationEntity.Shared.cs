26add9678eb8cf3a09469013623eb2fd9aba9a3b
Peter <petergorman11@gmail.com>
2022-07-30 14:22:21 -0400

    Update test items implementation

--- Server/Game/Shared/GameInformationEntity/GameInformationEntity.Shared.cs
+++ Server/Game/Shared/GameInformationEntity/GameInformationEntity.Shared.cs
@@ -1,4 +1,5 @@
 using CustomNetworking.Shared.Entities;
+using CustomNetworking.Shared.Networkables;
 using CustomNetworking.Shared.Networkables.Builtin;
 
 namespace CustomNetworking.Game;
@@ -10,15 +11,22 @@ public partial class GameInformationEntity : NetworkEntity
 		get => _testItems;
 		set
 		{
+			_testItems.Changed -= OnTestItemsChanged;
 			_testItems = value;
-#if SERVER
-			TriggerNetworkingChange( nameof(TestItems) );
-#endif
+			value.Changed += OnTestItemsChanged;
+			OnTestItemsChanged( value );
 		}
 	}
 	private NetworkedList<NetworkedInt> _testItems = new();
 
 	public GameInformationEntity( int entityId ) : base( entityId )
 	{
+	}
+	
+	protected virtual void OnTestItemsChanged( INetworkable networkable )
+	{
+#if SERVER
+		TriggerNetworkingChange( nameof(Position) );
+#endif
 	}
 }
