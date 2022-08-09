43241d6c5c93676cb4074732e17d8dcca492a50f
Peter <petergorman11@gmail.com>
2022-08-09 00:13:18 -0400

    Add EntityManager.Create( string, int ) for client-side creation

--- Server/Shared/Entities/EntityManager/EntityManager.Client.cs
+++ Server/Shared/Entities/EntityManager/EntityManager.Client.cs
@@ -7,6 +7,23 @@ namespace CustomNetworking.Shared;
 public partial class EntityManager
 {
 	/// <summary>
+	/// Creates and adds a new <see cref="IEntity"/> to this <see cref="EntityManager"/>.
+	/// </summary>
+	/// <param name="entityClass">The class name of the <see cref="IEntity"/> to create.</param>
+	/// <param name="entityId">The <see cref="IEntity"/>.<see cref="IEntity.EntityId"/> of the created <see cref="IEntity"/>.</param>
+	public void Create( string entityClass, int entityId )
+	{
+		var entityType = TypeHelper.GetTypeByName( entityClass );
+		if ( entityType is null || !entityType.IsClass || !entityType.IsAssignableTo( typeof(IEntity) ) )
+		{
+			Log.Error( $"Failed to create entity (type is not a class that implements {nameof(IEntity)})." );
+			return;
+		}
+
+		CreateInternal<IEntity>( entityId, entityType );
+	}
+	
+	/// <summary>
 	/// Deserializes an instance of <see cref="IEntity"/> and adds it to this <see cref="EntityManager"/>.
 	/// </summary>
 	/// <param name="reader">The reader to read the <see cref="IEntity"/> from.</param>
