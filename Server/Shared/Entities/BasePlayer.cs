4d5c3cae06c28b04cf2b526cc2fae3ca4e8d2ab1
Peter <petergorman11@gmail.com>
2022-08-09 00:21:24 -0400

    Collapse BasePlayer to a single file

--- Server/Shared/Entities/BasePlayer.cs
+++ Server/Shared/Entities/BasePlayer.cs
@@ -2,6 +2,7 @@ using CustomNetworking.Shared.Networkables;
 using CustomNetworking.Shared.Networkables.Builtin;
 using CustomNetworking.Shared.Utility;
 #if CLIENT
+using CustomNetworking.Client;
 using Sandbox;
 #endif
 
@@ -12,9 +13,18 @@ public partial class BasePlayer : IEntity
 	public event INetworkable<IEntity>.ChangedEventHandler? Changed;
 	public NetworkedInt EntityId { get; }
 	
+#if CLIENT
+	protected readonly TestPlayer PlayerPawn;
+#endif
+	
 	public BasePlayer( int entityId )
 	{
 		EntityId = entityId;
+		
+#if CLIENT
+		PlayerPawn = new TestPlayer();
+		Local.Client.Pawn = PlayerPawn;
+#endif
 	}
 	
 	public virtual void Deserialize( NetworkReader reader )
