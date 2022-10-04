ecf81daa261fb62148d128a0d97b8dbec9b74445
Peter <petergorman11@gmail.com>
2022-10-04 15:47:57 -0400

    Remove server monitor
    This will be re-visited later down the road

--- Monitor/code/Shared/Client/NetworkClient/NetworkClient.Shared.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CustomNetworking.Shared;
-
-/// <summary>
-/// Base class for any non-bot clients connected to a server.
-/// </summary>
-public partial class NetworkClient : INetworkClient
-{
-	public long ClientId { get; }
-}
